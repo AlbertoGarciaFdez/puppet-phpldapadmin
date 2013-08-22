68d79cbf8e31047281b1fff2550615518ba2dd1f
Alberto Garcia <alberto.garcia@rentalia.com>
2013-08-22 14:25:17 +0200

    Fixing stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,21 +2,42 @@ class phpldapadmin (
   $nginx_user = params_lookup( 'nginx_user' ),
   $base_dn    = params_lookup( 'base_dn' ),
   $root_dn    = params_lookup( 'root_dn' ),
+  $ip_server  = params_lookup( 'ip_server' )
 
 ) inherits phpldapadmin::params {
 
-  package { 'phpldapadmin':
-    ensure => installed,
+  file { 'responses':
+    path    => '/tmp/responses',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template("phpldapadmin/selections.erb")
   }
 
-  file { 'config.php':
-    path    => '/etc/phpldapadmin/config.php',
-    owner   => $nginx_user,
-    group   => $nginx_user,
-    mode    => '0640',
-    content => template("phpldapadmin/config.php.erb"),
-    require => Package['phpldapadmin'],
-    notify  => Service['nginx'],
+  package { 'php5-common':
+    ensure  => '5.4.4-14+deb7u3',
+    require => File['responses'],
+  }
+  
+  package { 'php5-fpm':
+    ensure  => '5.4.4-14+deb7u3',
+    require => Package['php5-common'],
+  }
+
+ package { 'php5-cli':
+    ensure  => '5.4.4-14+deb7u3',
+    require => Package['php5-fpm'],
+  }
+
+  package { 'php5-ldap':
+    ensure  => '5.4.4-14+deb7u3',
+    require => Package['php5-cli'],
+  }
+ 
+  package { 'phpldapadmin':
+    ensure        => installed,
+    responsefile  => '/tmp/responses',
+    require       => Package['php5-ldap'],
   }
 
   file { 'autofsMap.xml':
@@ -26,9 +47,5 @@ class phpldapadmin (
     mode    => '0640',
     source  => 'puppet:///modules/phpldapadmin/autofsMap.xml',
     require => Package['phpldapadmin'],
-  }
-
-  service { 'nginx':
-    ensure => running
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,4 +2,5 @@ class phpldapadmin::params {
   $nginx_user	= 'www-data'
   $base_dn		= 'dc=example,dc=org'
   $root_dn		= 'cn=admin,dc=example,dc=org'
+  $ip_server  = '127.0.0.1'
 }	
